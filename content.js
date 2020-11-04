e00716899357cefcfb96f388ed31502df08560b0
Oliver Ruste Jahren <oliverrj@Majestet.home>
2020-11-04 19:44:53 +0100

    improve layout

--- content.js
+++ content.js
@@ -1,6 +1,6 @@
 let hentAppend = () => {
     let tid = new Date();
-    let str = "<p id=\"timestamp\"> Synced date: " + (tid.getDate()<10?'0':'') + tid.getDate() + "." + (tid.getMonth()+1) + ", time: " + tid.getHours() + ":" + (tid.getMinutes()<10?'0':'') + tid.getMinutes() + "</p>";
+    let str = "<p id=\"timestamp\" style=\"margin-top: 10vw;\"> Synced date: " + (tid.getDate()<10?'0':'') + tid.getDate() + "." + (tid.getMonth()+1) + ", time: " + tid.getHours() + ":" + (tid.getMinutes()<10?'0':'') + tid.getMinutes() + "</p>";
     //let ret = document.createTextNode(str)
     return str;// ret;
 }
