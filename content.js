6da4ff66b8dbb56d2f3dd6531499fa48fc4ecb05
Oliver Ruste Jahren <oliverrj@Majestet.home>
2020-11-04 18:25:13 +0100

    push v.03

--- content.js
+++ content.js
@@ -1,11 +1,11 @@
 let hentAppend = () => {
     let tid = new Date();
-    let str = "\nSynced date: " + (tid.getDate()<10?'0':'') + tid.getDate() + "." + (tid.getMonth()+1) + ", time: " + tid.getHours() + ":" + (tid.getMinutes()<10?'0':'') + tid.getMinutes();
-    let ret = document.createTextNode(str)
-    return ret;
+    let str = "<p id=\"timestamp\"> Synced date: " + (tid.getDate()<10?'0':'') + tid.getDate() + "." + (tid.getMonth()+1) + ", time: " + tid.getHours() + ":" + (tid.getMinutes()<10?'0':'') + tid.getMinutes() + "</p>";
+    //let ret = document.createTextNode(str)
+    return str;// ret;
 }
 
 //document.addEventListener('DOMContentLoaded', function() {
-    let elt = document.getElementsByClassName("linkinfo")[0];
-    elt.appendChild(hentAppend());
+let elt = document.getElementsByClassName("linkinfo")[0];
+elt.innerHTML += hentAppend();//elt.appendChild(hentAppend());
   //}, false);
